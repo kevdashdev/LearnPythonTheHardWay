c994ff7a78a9a14ec079294ff2dfaf175abe2ff0
kevdashdev <kevdashdev@gmail.com>
2013-07-11 11:59:14 +0800

    Finish exercise 5

---
+++ ex5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+name = 'Kevin M. Candelaria'
+age = 21
+height = 66
+weight = 170
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
